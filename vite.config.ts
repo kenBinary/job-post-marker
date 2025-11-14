322cdae9731e57dfc42abc1214339c277405564b
ken <joshuabecaro@gmail.com>
2025-11-14 23:09:31 +0800

    refactor: rename main.ts to content.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: path.resolve(__dirname, "index.html"),
-        content: path.resolve(__dirname, "src/main.ts"),
+        content: path.resolve(__dirname, "src/content.ts"),
       },
       output: {
         entryFileNames: "[name].js",
