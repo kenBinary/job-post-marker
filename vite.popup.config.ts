68b715ed33eebc2ac7d3ae52b494be1324ae647d
ken <joshuabecaro@gmail.com>
2025-12-07 21:23:52 +0800

    feat: add new vite config

---
+++ vite.popup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    emptyOutDir: true,
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+});
