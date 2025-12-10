a4f72d15228adfe1a92f39716a571e89392920d3
ken <joshuabecaro@gmail.com>
2025-12-10 21:24:14 +0800

    feat: add aut mark state for linkedin

--- src/services/background.ts
+++ src/services/background.ts
@@ -1,4 +1,8 @@
-import type { OLJ_AUTO_MARK_TOGGLE_STATE } from "../types/chrome-storage-local.types";
+import { autoMarkState } from "../constants/autoMarkStateKeys";
+import type {
+  LINKEDIN_AUTO_MARK_TOGGLE_STATE,
+  OLJ_AUTO_MARK_TOGGLE_STATE,
+} from "../types/chrome-storage-local.types";
 import { setToLocal } from "../utils/chromeStorage";
 
 chrome.runtime.onInstalled.addListener(async () => {
@@ -6,8 +10,10 @@ chrome.runtime.onInstalled.addListener(async () => {
     oljAutoMarkToggleState: false,
   };
   try {
+    await setToLocal<LINKEDIN_AUTO_MARK_TOGGLE_STATE>({
+      [autoMarkState.linkedin]: false,
+    });
     await setToLocal<OLJ_AUTO_MARK_TOGGLE_STATE>(initialData);
-    console.log("Initial data set in chrome.storage.local:", initialData);
   } catch (error) {
     console.error("Error setting initial data in chrome.storage.local:", error);
   }
