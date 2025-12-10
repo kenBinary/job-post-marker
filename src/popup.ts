71fb601ab02049c638a4dc06160ae39a9debf20f
ken <joshuabecaro@gmail.com>
2025-12-10 21:24:57 +0800

    feat: add LinkedIn auto-mark toggle in popup

--- src/popup.ts
+++ src/popup.ts
@@ -1,3 +1,5 @@
+import { linkedinAutoMarkerSetup } from "./popup/linkedin/linkedinAutoMarker";
 import { oljAutoMarkerSetup } from "./popup/olj/oljAutoMarker";
 
 await oljAutoMarkerSetup();
+await linkedinAutoMarkerSetup();
