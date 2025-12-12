ab6e430b7de4e8584a1209fdc947024017a94bd8
ken <joshuabecaro@gmail.com>
2025-12-12 23:10:21 +0800

    feat: add Indeed auto marker toggle functionality

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,7 @@
+import { indeedAutoMarkerSetup } from "./popup/indeed/indeedAutoMarker";
 import { linkedinAutoMarkerSetup } from "./popup/linkedin/linkedinAutoMarker";
 import { oljAutoMarkerSetup } from "./popup/olj/oljAutoMarker";
 
 await oljAutoMarkerSetup();
 await linkedinAutoMarkerSetup();
+await indeedAutoMarkerSetup();
---
+++ src/popup/indeed/indeedAutoMarker.ts
@@ -0,0 +1,30 @@
+import { autoMarkState } from "../../constants/autoMarkStateKeys";
+import type { INDEED_AUTO_MARK_TOGGLE_STATE } from "../../types/chrome-storage-local.types";
+import { getFromLocal, setToLocal } from "../../utils/chromeStorage";
+
+export async function indeedAutoMarkerSetup() {
+  const initAutoToggleState = await getFromLocal<INDEED_AUTO_MARK_TOGGLE_STATE>(
+    [autoMarkState.indeed]
+  );
+
+  const indeedAutoMarkToggle = document.getElementById(
+    "indeed-auto-mark-toggle"
+  )! as HTMLInputElement;
+  const initState = initAutoToggleState[autoMarkState.indeed];
+  if (initState !== undefined) {
+    indeedAutoMarkToggle.checked = initState;
+  }
+
+  indeedAutoMarkToggle.addEventListener("change", async () => {
+    const toggleState = await getFromLocal<INDEED_AUTO_MARK_TOGGLE_STATE>([
+      autoMarkState.indeed,
+    ]);
+
+    if (toggleState.indeedAutoMarkToggleState !== undefined) {
+      await setToLocal<INDEED_AUTO_MARK_TOGGLE_STATE>({
+        [autoMarkState.indeed]: !toggleState.indeedAutoMarkToggleState,
+      });
+      indeedAutoMarkToggle.checked = !toggleState.indeedAutoMarkToggleState;
+    }
+  });
+}
