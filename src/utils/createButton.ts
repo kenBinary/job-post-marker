625d94f3eec8b4f77c370c97f302e7def0777867
ken <joshuabecaro@gmail.com>
2025-12-13 14:16:14 +0800

    feat: implement createMarkingButton utility

---
+++ src/utils/createButton.ts
@@ -0,0 +1,48 @@
+import { LOCAL_STORAGE_KEY_PREFIX } from "../constants/keyPrefixes";
+import { LOCAL_STORAGE_VALUE } from "../constants/keyValues";
+import { markAsViewed, unmarkAsViewed } from "./buttonMarkers";
+
+export function createMarkingButton(jobId: string): HTMLButtonElement {
+  const jobKey = `${LOCAL_STORAGE_KEY_PREFIX.JOBSTREET_JOB_PREFIX}${jobId}`;
+  const jobState = localStorage.getItem(jobKey);
+  if (!jobState) {
+    localStorage.setItem(jobKey, LOCAL_STORAGE_VALUE.NOT_VIEWED);
+  }
+  const button = document.createElement("button");
+  button.textContent = "Mark Viewed";
+  button.className = "marked-as-viewed-btn";
+  button.setAttribute("data-job-key", jobKey);
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const currentButton = event.currentTarget as HTMLButtonElement;
+    const jobKey = currentButton.getAttribute("data-job-key");
+    if (!jobKey) {
+      return;
+    }
+
+    const jobState = localStorage.getItem(jobKey);
+    if (!jobState) {
+      return;
+    }
+    currentButton.setAttribute("data-job-value", jobState);
+    if (!jobState) {
+      return;
+    }
+
+    if (jobState === LOCAL_STORAGE_VALUE.NOT_VIEWED) {
+      markAsViewed(currentButton);
+    } else {
+      unmarkAsViewed(currentButton);
+    }
+  });
+
+  if (jobState === LOCAL_STORAGE_VALUE.VIEWED) {
+    markAsViewed(button);
+  } else {
+    unmarkAsViewed(button);
+  }
+
+  return button;
+}
